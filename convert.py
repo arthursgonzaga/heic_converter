8f463eb1c6cb389817cff37c6124b0fbcbc331a5
Arthur Gonzaga <arthursgonzaga@gmail.com>
2025-04-05 11:46:59 -0300

    Refatorar função de conversão HEIC para suportar formatos de saída dinâmicos e melhorar a legibilidade

--- convert.py
+++ convert.py
@@ -14,33 +14,14 @@ def convert_heic_to(image_path, output_format="jpeg"):
     if output_format.lower() not in ['jpeg', 'png']:
         raise ValueError("Formato de saída deve ser 'jpeg' ou 'png'.")
 
-    # Carrega o arquivo HEIC
-    heif_file = pyheif.read(image_path)
-    image = Image.frombytes(
-        heif_file.mode,
-        heif_file.size,
-        heif_file.data,
-        "raw",
-        heif_file.mode
-    )
-
-    # Define nome do novo arquivo
-    output_path = os.path.splitext(image_path)[0] + f".{output_format}"
-
-    # Salva no novo formato
-    image.save(output_path, format=output_format.upper())
-    print(f"Imagem convertida: {output_path}")
-
-def convert_heic_2(image_path):
     pillow_heif.register_heif_opener()
 
     image = Image.open(image_path)
-    image_path = os.path.splitext(image_path)[0] + ".jpeg"
-    print(image_path)
-    image.save(image_path, format="JPEG")
+    image_path = f"{os.path.splitext(image_path)[0]}.{output_format}"
+    image.save(image_path, format=output_format.upper())
+    print(f"Imagem convertida: {image_path}")   
 
 if __name__ == "__main__":
     file_path = input("Digite o caminho do arquivo HEIC: ")
     output_format = input("Digite o formato de saída (jpeg/png): ")
-    # convert_heic_to(file_path, output_format)
-    convert_heic_2(file_path)
+    convert_heic_to(file_path, output_format)
